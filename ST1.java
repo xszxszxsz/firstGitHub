91136c5db73571dc7c305890186699eb11602ef7
xszxszxsz <46466461+xszxszxsz@users.noreply.github.com>
2019-01-08 09:28:57 +0800

    Add files via upload

--- ST1.java
+++ ST1.java
@@ -6,10 +6,10 @@ public class ST1 {
 	Scanner input=new Scanner(System.in);
 	System.out.print("请输入用户名：");
 	String name=input.next();
-	if(!name.equals("青")){
+	if(!name.equals("GitHub")){
 		System.out.println("对不起，你不是青");
 	}else{
-		System.out.println("欢迎你，青！");
+		System.out.println("欢迎你，GitHub！");
 	}
 	}
 
